04bcf1c36e7c0df3622e6d15c9f34e169a80f2f2
lks6123 <lks6123@163.com>
2017-04-30 11:49:14 +0800

    demo完成

---
+++ app/src/main/java/com/example/lks61/mytest/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.lks61.mytest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.zxing.activity.CaptureActivity;
+import com.example.zxing.encoding.EncodingUtils;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView resultTextView;
+    private EditText qrStrEditText;
+    private ImageView qrImgImageView;
+    private CheckBox mCheckBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
+        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
+        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
+        mCheckBox = (CheckBox) findViewById(R.id.logo);
+        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},1);
+                }else {
+                    startCaptureActivity();
+                }
+            }
+        });
+
+        Button generateQRCodeButton = (Button) this.findViewById(R.id.btn_add_qrcode);
+        generateQRCodeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String contentString = qrStrEditText.getText().toString();
+                if (!contentString.equals("")) {
+                    //根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
+                    Bitmap qrCodeBitmap = EncodingUtils.createQRCode(contentString, 350, 350,
+                            mCheckBox.isChecked() ?
+                                    BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher) :
+                                    null);
+                    qrImgImageView.setImageBitmap(qrCodeBitmap);
+                } else {
+                    Toast.makeText(MainActivity.this, "Text can not be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void startCaptureActivity() {
+        //打开扫描界面扫描条形码或二维码
+        Intent openCameraIntent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(openCameraIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            resultTextView.setText(scanResult);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0&&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startCaptureActivity();
+                }else {
+                    Toast.makeText(this, R.string.camera_permisson, Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+}
